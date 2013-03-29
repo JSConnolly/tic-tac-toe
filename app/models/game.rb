7abac1c48d7d1d1ae4768036ca23a4432ff2d2c0
J Connolly <jsconnolly1@gmail.com>
2013-03-29 11:09:10 -0700

    add procfile for heroku

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,8 @@ class Game < ActiveRecord::Base
   belongs_to :player_1, :class_name => 'Player', :foreign_key => 'player_1'
   belongs_to :player_2, :class_name => 'Player', :foreign_key => 'player_2'
 
-  # t.integer :player_1
-  # t.integer :player_2
+  # t.integer :player_1_id
+  # t.integer :player_2_id
   # t.string :board
   # t.integer :winner
 
