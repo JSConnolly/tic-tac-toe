66158ef41f0dd07dc9464209cb50cec195f28724
Gavin St. Ours <charmcitygavin@gmail.com>
2013-03-29 11:06:14 -0700

    Added a little pseudocode to spice things up.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,8 @@
 get '/' do
-  # Look in app/views/index.erb
+  # Display a list of available games or create a new game.
+  # Players who create a game become Player 1 (X) and players who
+  # join an existing game become Player 2 (O).
+
   erb :index
 end
 
