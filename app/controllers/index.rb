ba7bf793f7720512c2a20d6a1681b041acf82708
J Connolly <jsconnolly1@gmail.com>
2013-03-29 11:55:42 -0700

    a couple more rudimentary routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,10 +8,21 @@ end
 
 post '/game' do
 	p = Player.create( username: 'dummy' )
-  Game.create( player_1_id: p.id, board: '000000000')
+  g = Game.create( player_1_id: p.id, board: '000000000')
 	
-	erb :game
-end	
+  redirect "/game/#{g.id}"
+end
+
+get '/game/:id' do
+  #make sure the user is a player in this game, if not redirect
+  erb :game
+end
+
+get '/game/join/:id' do
+  # add the current user to the game
+  Game#.change_attr('player_2_id', current_user.id)
+  redirect 'game/:id'
+end
 
 post '/move' do
 	send_move
