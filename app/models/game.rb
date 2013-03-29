e09a510d9cfd2fd7aff408b4b6660006db84055f
J Connolly <jsconnolly1@gmail.com>
2013-03-29 11:04:29 -0700

    working activerecord relations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,10 @@
 class Game < ActiveRecord::Base
   belongs_to :player_1, :class_name => 'Player', :foreign_key => 'player_1'
   belongs_to :player_2, :class_name => 'Player', :foreign_key => 'player_2'
+
+  # t.integer :player_1
+  # t.integer :player_2
+  # t.string :board
+  # t.integer :winner
+
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,15 @@
 class Player < ActiveRecord::Base
-  has_many :games
+  has_many :games_as_p1, :class_name => "Game", :foreign_key => 'player_1_id'
+  has_many :games_as_p2, :class_name => "Game", :foreign_key => 'player_2_id'
+  # create_table :players do |t|
+  # t.string :username
+  # t.string :email
+  # t.string :status
+  # t.string :password_hash
+  
+  def games
+    self.games_as_p1 + self.games_as_p2
+  end
+
+
 end
